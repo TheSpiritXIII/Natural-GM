aab828e76af6b92236fc86c9667eccdcf6d57d2a
Joshua Spayd <jds9496@gmail.com>
2013-08-23 15:18:22 -0500

    Added tab closing
    I added the closing of tabs and changed the indentation from spaces to
    tabs.

--- Source/Managers/WindowManager.hpp
+++ Source/Managers/WindowManager.hpp
@@ -2,22 +2,22 @@
  *  @file WindowManager.hpp
  *  @section License
  *
- *      Copyright (C) 2013 Daniel Hrabovcak
+ *	  Copyright (C) 2013 Daniel Hrabovcak
  *
- *      This file is a part of the Natural GM IDE.
+ *	  This file is a part of the Natural GM IDE.
  *
- *      This program is free software: you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation, either version 3 of the License, or
- *      (at your option) any later version.
+ *	  This program is free software: you can redistribute it and/or modify
+ *	  it under the terms of the GNU General Public License as published by
+ *	  the Free Software Foundation, either version 3 of the License, or
+ *	  (at your option) any later version.
  *
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
+ *	  This program is distributed in the hope that it will be useful,
+ *	  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	  GNU General Public License for more details.
  *
- *      You should have received a copy of the GNU General Public License
- *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *	  You should have received a copy of the GNU General Public License
+ *	  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 #pragma once
 #ifndef _NGM_WINDOWMANAGER_HPP
@@ -33,203 +33,203 @@
 
 namespace NGM
 {
-    class MainWindow;
+	class MainWindow;
 
-    namespace Model
-    {
-        class ResourceItemModel;
-        class ResourceProjectItem;
-    }
+	namespace Model
+	{
+		class ResourceItemModel;
+		class ResourceProjectItem;
+	}
 
-    namespace Widget
-    {
-        class ResourceTabWidget;
-        class ResourceSplitter;
-    }
+	namespace Widget
+	{
+		class ResourceTabWidget;
+		class ResourceSplitter;
+	}
 
-    namespace Resource
-    {
-        class Widget;
-    }
+	namespace Resource
+	{
+		class Widget;
+	}
 
-    namespace Manager
-    {
-        class ResourceManager;
-        class ProjectManager;
-        class ActionManager;
+	namespace Manager
+	{
+		class ResourceManager;
+		class ProjectManager;
+		class ActionManager;
 
-        /**************************************************//*!
-        *	@brief	Creates Windows and manages all other
-        *			resources and managers.
-        ******************************************************/
-        class WindowManager : public QApplication
-        {
-            Q_OBJECT
+		/**************************************************//*!
+		*	@brief	Creates Windows and manages all other
+		*			resources and managers.
+		******************************************************/
+		class WindowManager : public QApplication
+		{
+			Q_OBJECT
 
-        public:
+		public:
 
-            /**************************************************//*!
-            *	@brief	Creates a single window.
-            ******************************************************/
-            WindowManager(int argc, char *argv[]);
+			/**************************************************//*!
+			*	@brief	Creates a single window.
+			******************************************************/
+			WindowManager(int argc, char *argv[]);
 
-            /**************************************************//*!
-            *	@brief	Destroys all windows.
-            ******************************************************/
-            ~WindowManager();
+			/**************************************************//*!
+			*	@brief	Destroys all windows.
+			******************************************************/
+			~WindowManager();
 
-            /**************************************************//*!
-            *	@brief	Contains all project metadata.
-            ******************************************************/
-            Manager::ProjectManager projectManager;
+			/**************************************************//*!
+			*	@brief	Contains all project metadata.
+			******************************************************/
+			Manager::ProjectManager projectManager;
 
-            /**************************************************//*!
-            *	@brief	Contains all actions and icons.
-            ******************************************************/
-            Manager::ActionManager actionManager;
+			/**************************************************//*!
+			*	@brief	Contains all actions and icons.
+			******************************************************/
+			Manager::ActionManager actionManager;
 
-            /**************************************************//*!
-            *	@brief	Contains all settings.
-            ******************************************************/
-            Manager::SettingManager settingManager;
+			/**************************************************//*!
+			*	@brief	Contains all settings.
+			******************************************************/
+			Manager::SettingManager settingManager;
 
-            /**************************************************//*!
-            *	@brief	Adds and returns a new and empty window.
-            ******************************************************/
-            MainWindow *addWindow();
+			/**************************************************//*!
+			*	@brief	Adds and returns a new and empty window.
+			******************************************************/
+			MainWindow *addWindow();
 
-            /**************************************************//*!
-            *	@brief	Permanantely destroys the indicated window.
-            ******************************************************/
-            void removeWindow(MainWindow *window);
+			/**************************************************//*!
+			*	@brief	Permanantely destroys the indicated window.
+			******************************************************/
+			void removeWindow(MainWindow *window);
 
-            /**************************************************//*!
-            *	@brief	Adds a new project to the heirachy model.
-            ******************************************************/
-            void addProject(NGM::Model::ResourceProjectItem *project);
+			/**************************************************//*!
+			*	@brief	Adds a new project to the heirachy model.
+			******************************************************/
+			void addProject(NGM::Model::ResourceProjectItem *project);
 
-        protected:
+		protected:
 
-            /**************************************************//*!
-            *	@brief	Contains all loaded projects and resources.
-            ******************************************************/
-            Model::ResourceItemModel *heirarchy;
+			/**************************************************//*!
+			*	@brief	Contains all loaded projects and resources.
+			******************************************************/
+			Model::ResourceItemModel *heirarchy;
 
-            /**************************************************//*!
-            *	@brief	Creates a new project with user input.
-            ******************************************************/
-            void createProjectDialog();
+			/**************************************************//*!
+			*	@brief	Creates a new project with user input.
+			******************************************************/
+			void createProjectDialog();
 
-            /**************************************************//*!
-            *	@brief	Opens an existing project with user input.
-            ******************************************************/
-            void openProjectDialog();
+			/**************************************************//*!
+			*	@brief	Opens an existing project with user input.
+			******************************************************/
+			void openProjectDialog();
 
-            /**************************************************//*!
-            *	@brief	Filters keyboard shortcuts.
-            ******************************************************/
-            bool eventFilter(QObject*object, QEvent *event);
+			/**************************************************//*!
+			*	@brief	Filters keyboard shortcuts.
+			******************************************************/
+			bool eventFilter(QObject*object, QEvent *event);
 
-            friend class NGM::MainWindow;
-            friend class Manager::ActionManager;
+			friend class NGM::MainWindow;
+			friend class Manager::ActionManager;
 
-        protected:
+		protected:
 
-            /**************************************************//*!
-            *	@brief	Activates cut/copy actions.
-            ******************************************************/
-            void canCopy(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates cut/copy actions.
+			******************************************************/
+			void canCopy(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the paste action.
-            ******************************************************/
-            void canPaste(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the paste action.
+			******************************************************/
+			void canPaste(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the select action.
-            ******************************************************/
-            void canSelect(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the select action.
+			******************************************************/
+			void canSelect(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the undo action.
-            ******************************************************/
-            void canUndo(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the undo action.
+			******************************************************/
+			void canUndo(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the redo action.
-            ******************************************************/
-            void canRedo(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the redo action.
+			******************************************************/
+			void canRedo(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the zoom in action.
-            ******************************************************/
-            void canZoomIn(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the zoom in action.
+			******************************************************/
+			void canZoomIn(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the zoom out action.
-            ******************************************************/
-            void canZoomOut(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the zoom out action.
+			******************************************************/
+			void canZoomOut(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the zoom action.
-            ******************************************************/
-            void canZoom(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the zoom action.
+			******************************************************/
+			void canZoom(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Activates the save actions.
-            ******************************************************/
-            void isModified(const bool &value);
+			/**************************************************//*!
+			*	@brief	Activates the save actions.
+			******************************************************/
+			void isModified(const bool &value);
 
-            /**************************************************//*!
-            *	@brief	Sets the current resource widget.
-            ******************************************************/
-            void setResourceWidget(Resource::Widget *widget);
+			/**************************************************//*!
+			*	@brief	Sets the current resource widget.
+			******************************************************/
+			void setResourceWidget(Resource::Widget *widget);
 
-            friend class Widget::ResourceSplitter;
+			friend class Widget::ResourceSplitter;
 
-        private:
+		private:
 
-            /**************************************************//*!
-            *	@brief	Stores all created windows.
-            ******************************************************/
-            std::list<MainWindow*> windows;
+			/**************************************************//*!
+			*	@brief	Stores all created windows.
+			******************************************************/
+			std::list<MainWindow*> windows;
 
-            /**************************************************//*!
-            *	@brief	Stores the current focused window.
-            ******************************************************/
-            MainWindow *currentWindow;
+			/**************************************************//*!
+			*	@brief	Stores the current focused window.
+			******************************************************/
+			MainWindow *currentWindow;
 
-            /**************************************************//*!
-            *	@brief	Stores the current focused window.
-            ******************************************************/
-            Widget::ResourceTabWidget *currentTabWidget;
+			/**************************************************//*!
+			*	@brief	Stores the current focused window.
+			******************************************************/
+			Widget::ResourceTabWidget *currentTabWidget;
 
-            /**************************************************//*!
-            *	@brief	Stores the currently focused widget.
-            ******************************************************/
-            Resource::Widget *resourceWidget;
+			/**************************************************//*!
+			*	@brief	Stores the currently focused widget.
+			******************************************************/
+			Resource::Widget *resourceWidget;
 
-            /**************************************************//*!
-            *	@brief	Stores a message for statusbars.
-            ******************************************************/
-            QString message;
+			/**************************************************//*!
+			*	@brief	Stores a message for statusbars.
+			******************************************************/
+			QString message;
 
-            /**************************************************//*!
-            *	@brief	Stores a message for statusbars.
-            ******************************************************/
-            int progressMax;
+			/**************************************************//*!
+			*	@brief	Stores a message for statusbars.
+			******************************************************/
+			int progressMax;
 
-            /**************************************************//*!
-            *	@brief	Stores a message for statusbars.
-            ******************************************************/
-            int progressMin;
+			/**************************************************//*!
+			*	@brief	Stores a message for statusbars.
+			******************************************************/
+			int progressMin;
 
-            /**************************************************//*!
-            *	@brief	Stores a message for statusbars.
-            ******************************************************/
-            int progressCurrent;
-        };
-    }
+			/**************************************************//*!
+			*	@brief	Stores a message for statusbars.
+			******************************************************/
+			int progressCurrent;
+		};
+	}
 }
 
 #endif // _NGM_WINDOWMANAGER__HPP
--- Source/Widgets/ResourceTab.cpp
+++ Source/Widgets/ResourceTab.cpp
@@ -3,22 +3,22 @@
  *
  *  @section License
  *
- *      Copyright (C) 2013 Daniel Hrabovcak
+ *	  Copyright (C) 2013 Daniel Hrabovcak
  *
- *      This file is a part of the Natural GM IDE.
+ *	  This file is a part of the Natural GM IDE.
  *
- *      This program is free software: you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation, either version 3 of the License, or
- *      (at your option) any later version.
+ *	  This program is free software: you can redistribute it and/or modify
+ *	  it under the terms of the GNU General Public License as published by
+ *	  the Free Software Foundation, either version 3 of the License, or
+ *	  (at your option) any later version.
  *
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
+ *	  This program is distributed in the hope that it will be useful,
+ *	  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	  GNU General Public License for more details.
  *
- *      You should have received a copy of the GNU General Public License
- *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *	  You should have received a copy of the GNU General Public License
+ *	  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 #include "ResourceTab.hpp"
 #include "../Resources/Widget.hpp"
@@ -27,6 +27,7 @@
 #include "WindowManager.hpp"
 #include <QMenu>
 #include <QDebug>
+#include <QMessageBox>
 using std::list;
 using std::pair;
 
@@ -83,6 +84,8 @@ namespace NGM
 				menu->addAction("Move Next Window");*/
 				menu->popup(this->mapToGlobal(point));
 			});
+
+			connect(this, &ResourceTab::tabCloseRequested, this, &ResourceTab::closeTab);
 		}
 
 		Resource::Widget *ResourceTab::resourceOpen(Model::ResourceBaseItem *resource)
@@ -134,6 +137,15 @@ namespace NGM
 		void ResourceTab::changeEvent(QEvent *event)
 		{
 			QTabWidget::changeEvent(event);
+		}
+
+		void ResourceTab::closeTab(int ind)
+		{
+			QMessageBox::StandardButton reply;
+			reply = QMessageBox::question(this, "", "Close tab " + tabText(ind) + "?", QMessageBox::Yes|QMessageBox::No);
+
+			if (reply == QMessageBox::Yes)
+				removeTab(ind);
 		}
 
 		void ResourceTab::modifedWidget(const bool &modified)
